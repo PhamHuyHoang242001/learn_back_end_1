0bd4a7cd670b0c1db72126325b4b95c3a5412a1c
huyhoang242001 <huyhoang.pham@secomus.com>
2023-12-20 15:09:48 +0700

    ddd

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
-const authorRoute = require("./routes/author");
-const bookRoute = require("./routes/book");
-const userRoute = require("./routes/user");
-const borrowRoute = require("./routes/borrow");
+import express from "express";
+// const mongoose = require("mongoose");
+import morgan from "morgan";
+import dotenv from "dotenv";
 
 dotenv.config();
+const app = express();
+// //Connect MongoDB
+// mongoose.connect((process.env.MONGODB_URL), { useNewUrlParser: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to MongoDB', error);
+//   });
 
-//Connect MongoDB
-mongoose.connect((process.env.MONGODB_URL), { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB', error);
-  });
-
-app.use(bodyParser.json({limit:"50mb"}));
-app.use(cors());
+// app.use(bodyParser.json({ limit: "50mb" }));
+// app.use(cors());
 app.use(morgan("common"));
 
 app.get("/api", (req, res) => {
-    res.status(200).json("Hello");
-})
+  res.status(200).json("Hello");
+});
+app.get("/welcom", (req, res) => {
+  res.status(200).json("chào mọi");
+});
 
-//Routes
-app.use("/author", authorRoute);
-app.use("/book", bookRoute);
-app.use("/user", userRoute);
-app.use("/borrow", borrowRoute);
+// //Routes
+// app.use("/author", authorRoute);
+// app.use("/book", bookRoute);
+// app.use("/user", userRoute);
+// app.use("/borrow", borrowRoute);
 
 app.listen(8000, () => {
-    console.log("Server is running")
+  console.log("Server is running");
 });
